c6e913b4c350f55ec8165f2cbbff463c2ce49f78
Roodewald <pavel999977@gmail.com>
2021-02-15 23:32:55 +0500

    Inputs for 1 Player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D playerRb;
+
+    private float moveSpeed = 5f;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 moveInput = context.ReadValue<Vector2>();
+        Debug.Log(moveInput);
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
