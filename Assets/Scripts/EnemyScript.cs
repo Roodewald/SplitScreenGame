f66cafb37be3650eed5b85bae1bc334915c4aac0
Roodewald <pavel999977@gmail.com>
2021-02-16 21:37:08 +0500

    Add safe zone and Enemy(with bug need complex vetor 2)

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    [SerializeField] GameObject player1;
+    [SerializeField] GameObject player2;
+    private Rigidbody2D enemyRB;
+    private PlayerController player1Script;
+    private PlayerController player2Script;
+    private Vector2 spawnPosition;
+    private float moveSpeed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRB = GetComponent<Rigidbody2D>();
+        spawnPosition = transform.position;
+        //Get player script
+        player1Script = player1.GetComponent<PlayerController>();
+        player2Script = player2.GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(!player1Script.inSafeZone || !player2Script.inSafeZone)
+        {
+            if (!player1Script.inSafeZone)
+            {
+                enemyRB.MovePosition( player1.transform.position);
+            }
+            else if (!player2Script.inSafeZone)
+            {
+                enemyRB.MovePosition( player2.transform.position);
+            }
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,16 +8,32 @@ public class PlayerController : MonoBehaviour
     [SerializeField] Rigidbody2D playerRb;
 
     private float moveSpeed = 5f;
-    public Vector2 moveInput;
+    private Vector2 moveInput;
+
+    public bool inSafeZone;
 
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
-        Debug.Log(moveInput);
     }
 
     void FixedUpdate()
     {
         playerRb.MovePosition(playerRb.position + moveInput * moveSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("SafeZone"))
+        {
+            inSafeZone = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("SafeZone"))
+        {
+            inSafeZone = false;
+        }
     }
 }
